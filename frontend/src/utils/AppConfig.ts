cbe3e397349f97f81e1a4b72c1e4293f0a9b5c6d
Anna Sudol <anjasudol@gmail.com>
2022-12-08 21:23:37 +0000

    bridge

--- frontend/src/utils/AppConfig.ts
+++
@@ -1,8 +0,0 @@
-// FIXME: Update this configuration file based on your project information
-
-export const AppConfig = {
-  site_name: 'Cross chain bridge',
-  title: 'ETH-BSC Bridge',
-  description: 'Cross chain ETH-BSC bridge built in Nextjs and Tailwind CSS',
-  locale: 'en',
-};

8914a86cf70d762115ad533848fdd4bf7ac14dbc
Anna Sudol <anjasudol@gmail.com>
2022-12-11 16:53:55 +0000

    facet

---
+++ frontend/src/ui/Facet/index.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+export const Facet = () => {
+  const handleFaucet = async () => {};
+  return (
+    <div className="flex flex-col items-center p-10">
+      <Image src="/assets/airdrop.svg" width={100} height={100} alt="airdrop" />
+      <div className="mt-5 text-center text-sm text-gray-200">
+        <p className="p-2">
+          You can claim 1 bEth per wallet every 24 hours.
+          <br />
+          bEth faucet are available only on Goerli Testnet.
+        </p>
+      </div>
+      <button
+        onClick={handleFaucet}
+        className="items-center rounded-full border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-blue-900 shadow-sm hover:bg-green-200 focus:outline-none"
+      >
+        Receive 10 Port
+      </button>
+    </div>
+  );
+};
--- frontend/src/ui/Tabs/index.tsx
+++ frontend/src/ui/Tabs/index.tsx
@@ -1,11 +1,13 @@
 import { Tab } from '@headlessui/react';
 
+import { Facet } from '../Facet';
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
 export default function Tabs() {
-  const categories = ['Bridge', 'Buy tokens'];
+  const categories = ['Bridge', 'Facet'];
 
   return (
     <div className="mt-16 w-full max-w-xl py-16 sm:px-0">
@@ -16,11 +18,8 @@ export default function Tabs() {
               key={category}
               className={({ selected }) =>
                 classNames(
-                  'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700',
-                  'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                  selected
-                    ? 'bg-white shadow'
-                    : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
+                  'w-full rounded-lg py-2.5 text-sm font-medium leading-5',
+                  selected ? 'bg-blue-900 shadow text-white' : 'text-white'
                 )
               }
             >
@@ -33,39 +32,11 @@ export default function Tabs() {
             <Tab.Panel
               key={idx}
               className={classNames(
-                'rounded-xl bg-white p-3 h-96',
-                'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
+                'rounded-xl bg-blue-900 p-3 h-96',
+                'bg-blue-900 focus:outline-none'
               )}
             >
-              {Object.keys(categories)}
-              {/* <ul>
-                {posts.map((post) => (
-                  <li
-                    key={post.id}
-                    className="relative rounded-md p-3 hover:bg-gray-100"
-                  >
-                    <h3 className="text-sm font-medium leading-5">
-                      {post.title}
-                    </h3>
-
-                    <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                      <li>{post.date}</li>
-                      <li>&middot;</li>
-                      <li>{post.commentCount} comments</li>
-                      <li>&middot;</li>
-                      <li>{post.shareCount} shares</li>
-                    </ul>
-
-                    <a
-                      href="#"
-                      className={classNames(
-                        'absolute inset-0 rounded-md',
-                        'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'
-                      )}
-                    />
-                  </li>
-                ))}
-              </ul> */}
+              {idx === 1 ? <Facet /> : <p>bridge</p>}
             </Tab.Panel>
           ))}
         </Tab.Panels>

e1f6c51d25de6cbf12a3aaece123b7bfd45b3742
Anna Sudol <anjasudol@gmail.com>
2022-12-21 19:25:20 +0000

    tabs

---
+++ frontend/component/Tabs.tsx
@@ -0,0 +1,57 @@
+import { Tab } from '@headlessui/react'
+import { useState } from 'react'
+
+// import { useBridgeContract, useTokenContract } from '@/hooks/useContract';
+// import { Bridge } from '../Bridge'
+// import { Facet } from '../Facet'
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Tabs() {
+  // const { chain } = useNetwork();
+  // const chainId = chain?.id || 5;
+  const [contract, setContract] = useState()
+
+  const contractValues = async () => {}
+
+  contractValues()
+
+  const categories = ['Bridge', 'Facet']
+  return (
+    <div className="mt-16 w-full max-w-xl py-16 sm:px-0">
+      <Tab.Group>
+        <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+          {categories.map((category) => (
+            <Tab
+              key={category}
+              className={({ selected }) =>
+                classNames(
+                  'w-full rounded-lg py-2.5 text-sm font-medium leading-5',
+                  selected ? 'bg-blue-900 shadow text-white' : 'text-white'
+                )
+              }
+            >
+              {category}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels className="mt-2">
+          {categories.map((_cat, idx) => (
+            <Tab.Panel
+              key={idx}
+              className={classNames(
+                'rounded-xl bg-blue-900 p-3 h-96',
+                'bg-blue-900 focus:outline-none'
+              )}
+            >
+              <button onClick={contractValues}>clis</button>
+              {/* {idx === 1 ? <Facet /> : <Bridge />} */}
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
+  )
+}

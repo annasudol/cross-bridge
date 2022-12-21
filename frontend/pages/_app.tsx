be1fcd6e57cb4559729785c62d5d2665b6d445c0
Anna Sudol <anjasudol@gmail.com>
2022-12-21 19:19:28 +0000

    front updated

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,57 @@
 import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import { useEffect, useState } from 'react'
+import {
+  connectorsForWallets,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit'
+import '@rainbow-me/rainbowkit/styles.css'
 import type { AppProps } from 'next/app'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import { polygonMumbai, goerli } from 'wagmi/chains'
+const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || ''
+
+const { provider, webSocketProvider, chains } = configureChains(
+  [polygonMumbai, goerli],
+  [
+    alchemyProvider({
+      apiKey: ALCHEMY_API_KEY,
+    }),
+    publicProvider(),
+  ]
+)
+
+const { wallets } = getDefaultWallets({
+  appName: 'Cross Chain Bridge',
+  chains: [polygonMumbai, goerli],
+})
+
+const demoAppInfo = {
+  appName: 'Cross Chain Bridge',
+}
+
+const connectors = connectorsForWallets(wallets)
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => setMounted(true), [])
+  if (!mounted) return null
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider appInfo={demoAppInfo} chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
---
+++ frontend/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+    site_name: 'Bridge',
+    title: 'Cross-chain Bridge',
+    description: 'ETH-MATIC bridge built in Nextjs and Tailwind CSS',
+    locale: 'en',
+};
